59a4f09783004a8f4fa70c2416ef2c22f080fdad
Apiwit kami <apiwitkami@gmail.com>
2022-03-27 12:17:50 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:final_620710336/game_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Food',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+          ),
+          headline6: TextStyle(
+            fontSize: 22.0,
+            //fontWeight: FontWeight.bold,
+          ),
+          bodyText2: TextStyle(
+            fontSize: 14.0,
+          ),
+        ),
+      ),
+      home: const GamePage(),
+    );
+  }
+}
